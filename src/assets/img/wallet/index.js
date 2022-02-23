4d7e93ae20f8e3a3049d8a69459a069854a12e99
SullFurix <sull.furix@gmail.com>
2022-02-23 02:28:58 +0100

    wallet: set images

---
+++ src/assets/img/wallet/index.js
@@ -0,0 +1,13 @@
+import metamask from "./metamask.png";
+import walletconnect from "./walletconnect.png";
+import trustwallet from "./trustwallet.png";
+import ledger from "./ledger.png";
+
+const WalletLogo = {
+  MetaMask: metamask,
+  WalletConnect: walletconnect,
+  TrustWallet: trustwallet,
+  Ledger: ledger,
+};
+
+export default WalletLogo;
