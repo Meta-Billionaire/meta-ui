4bf8db61b576b50e797db1e1f9f84e54a43d8fe5
SullFurix <sull.furix@gmail.com>
2022-02-23 02:37:16 +0100

    token: import tokens img

---
+++ src/utils/assets/token/index.js
@@ -0,0 +1,11 @@
+import eth from "./eth.png";
+import rink from "./rink.png";
+import poly from "./poly.png";
+
+const AssetsLogo = {
+  Ethereum: eth,
+  Rinkeby: rink,
+  Polygon: poly,
+};
+
+export default AssetsLogo;
